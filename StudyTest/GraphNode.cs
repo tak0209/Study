bc371e75ec5d6b9c30566519ffb377df9ee5b2ee
tak0209 <tak0209@Hotmail.com>
2015-01-24 13:51:48 -0800

    Add BFD and DFS

---
+++ StudyTest/GraphNode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StudyTest
+{
+    public class GraphNode
+    {
+        public int Value { get; set; }
+        public bool Visited { get; set; }
+        public GraphNode[] Childern { get; set; }
+    }
+}
---
+++ TestCase/GraphTest.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StudyTest;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestCase
+{
+    [TestClass]
+    public class GraphTest
+    {
+        private static GraphNode SetupGraph()
+        {
+            GraphNode g = new GraphNode();
+            g.Value = 1;
+            g.Childern = new GraphNode[] { new GraphNode { Value = 2 }, new GraphNode { Value = 3 } };
+            g.Childern[0].Childern = new GraphNode[] { new GraphNode { Value = 5 }, new GraphNode { Value = 4 } };
+            g.Childern[0].Childern[1].Childern = new GraphNode[] { g.Childern[0].Childern[0] };
+            return g;
+        }
+
+        [TestMethod]
+        public void TestBFS()
+        {
+            //              1
+            //            /   \
+            //           2     3
+            //          /  \
+            //         5 -  4 
+
+            GraphNode g = SetupGraph();
+            Graph.BFS(g);
+        }
+
+        [TestMethod]
+        public void TestDFS()
+        {
+            GraphNode g = SetupGraph();
+            Graph.DFS(g);
+        }
+    }
+}
